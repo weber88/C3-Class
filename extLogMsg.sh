dda2a26cec38abab04e2eed31a906b859f898854
Weber, Joe <joe.weber@wwt.com>
2017-06-07 00:37:16 -0500

    example

--- extLogMsg.sh
+++ extLogMsg.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
  
 . /utils.sh
- 
+
+
+echo "CloudCenter_EXTERNAL_SERVICE_LOG_MSG_START"
+echo "$logMsg"
+echo "CloudCenter_EXTERNAL_SERVICE_LOG_MSG_END"
+
 print_log "Start Log"
  
 print_log "Message - $logMsg"
