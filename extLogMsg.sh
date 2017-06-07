526c2e29d76b01e7076265ac43670d48d2f7e97e
Weber, Joe <joe.weber@wwt.com>
2017-06-07 00:26:15 -0500

    example

--- extLogMsg.sh
+++ extLogMsg.sh
@@ -2,10 +2,15 @@
  
 . /utils.sh
  
-print_log "This is the Log message - $logMsg"
+print_log "This is a basic log message"
  
-result="{\"hostName\":\"testsite\",\"ipAddress\":\"10.1.1.5\",\"environment\":{\"hello\":\"world\",\"instanceName\":\"test_instance\",\"instanceType\":\"dummy\",\"serviceType\":\"custom\"}}"
-
-print_log "$result"
-
+result="hostName: testsite      #hostname parameter
+ipAddress: 10.1.1.5             #ipAddress parameter
+environment:
+   hello: world                 #key-value environment parameter
+   instanceName: test_instance  #key-value environment parameter
+   instanceType: dummy          #key-value environment parameter
+   serviceType: custom          #key-value environment parameter
+ 
+"
 print_ext_service_result "$result"
