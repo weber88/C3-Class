f43b9e333abc663a9cff8890107221d41070feda
Weber, Joe <joe.weber@wwt.com>
2017-06-07 00:01:04 -0500

    example

--- extLogMsg.sh
+++ extLogMsg.sh
@@ -5,10 +5,6 @@
 print_log "This is the Log message - $logMsg"
  
 result="environment:
-   hello: world                 #key-value environment parameter
-   instanceName: test_instance  #key-value environment parameter
-   instanceType: dummy          #key-value environment parameter
-   serviceType: custom          #key-value environment parameter
  
 "
 
