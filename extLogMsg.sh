5e7d5348d4260f5a61276aed50b5debb5bd05751
Joe Weber <joe.weber@wwt.com>
2017-06-15 12:11:28 -0500

    Create extLogMsg.sh

--- extLogMsg.sh
+++ extLogMsg.sh
@@ -4,16 +4,7 @@
  
 print_log "This is the Log message - $logMsg"
  
-result="hostName: testsite      #hostname parameter
-ipAddress: 10.1.1.5             #ipAddress parameter
-environment:
-   hello: world
-   instanceName: test_instance
-   instanceType: dummy
-   serviceType: custom
+result="{\"hostName\":\"testsite\",\"ipAddress\":\"10.1.1.5\",\"environment\":{\"hello\":\"world\",\"instanceName\":\"test_instance\",\"instanceType\":\"dummy\",\"serviceType\":\"custom\"}}"
  
-"
-
+print_ext_service_result "$result" 
 print_log "$result"
-
-print_ext_service_result "$result"
